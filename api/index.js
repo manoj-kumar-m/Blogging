692a2ef8a3803cb8f7af8f22baebf6429a1b772d
manoj-kumar-m <manojkumarm0136@gmail.com>
2022-06-01 17:13:28 +0530

    optimized for build

--- api/index.js
+++ api/index.js
@@ -42,6 +42,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("8000", () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Backend is running.");
 });
